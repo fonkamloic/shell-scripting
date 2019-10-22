c54a2cf81026c29b2a13e277134cf53b65495ca7
fonkamloic <fonkamloic@gmail.com>
2019-10-23 01:56:33 +0200

    Various shell scripts

---
+++ shell6.sh
@@ -0,0 +1,3 @@
+#Arithmetic operation
+c=29 a=30 b=2 d=5
+echo `expr $a \* \( $b + $c \) / $d`
---
+++ shell8.sh
@@ -0,0 +1,8 @@
+#Escape sequences
+echo "Hey world, \nWhat's up?"
+echo "\n\n"
+echo "Hey world, \rWhat's up?"
+echo "Hey world, \tWhat's up?"
+echo "\033[7mHey world, \b\bWhat's up?"
+echo "\033[1mwhat's up?\033[0m Really?"
+echo "\033[7m`sh -c help`"
